186d355bd71938a414c352e07862600d627f6cd5
Hayes Williams <hwpi31@gmail.com>
2010-04-30 02:34:27 -0700

    More updates to btrfsguitools.py. Fixed the README's nonupdatedness.

--- btrfsguitools.py
+++ btrfsguitools.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-import getopt
-import sys
-sys.path.append()
-interface = getopt.getopt(sys.argv[1:], '', ['interface='])[0][0][1]
-getattr(guisupport, interface)
+import getopt, sys
+class SQLAlchemyMagic:
+    """Contains the universial magic for storing snapshots"""
+    pass
+class GTKGUIInterface(SQLAlchemyMagic):
+    """Contains the GTK GUI code"""
+    pass
+class QtGUIInterface(SQLAlchemyMagic):
+    """Contains the Qt GUI code"""
+    pass
+UI = {'GTK': GTKGUIInterface, 'Qt': QtGUIInterface}
+UIClass = UI[getopt.getopt(sys.argv[1:], '', ['interface='])[0][0][1]]()
